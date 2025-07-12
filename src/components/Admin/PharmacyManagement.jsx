606b23d7d2812fff7afe86568c07cc60fea514b6
hamza-mqbl <hamzaaliics@gmail.com>
2025-07-12 20:45:56 +0500

    resolved the issue of placeholder styling in search field

--- src/components/Admin/PharmacyManagement.jsx
+++ src/components/Admin/PharmacyManagement.jsx
@@ -169,7 +169,7 @@ const PharmacyManagement = ({
               placeholder="Rechercher des pharmacies..."
               value={searchTerm}
               onChange={(e) => setSearchTerm(e.target.value)}
-              className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none text-sm lg:text-base"
+              className="w-full pl-10 pr-4 py-2 border border-gray-300 text-black rounded-lg focus:ring-2 focus:ring-blue-500 focus:border-blue-500 outline-none text-sm lg:text-base placeholder:text-black"
             />
           </div>
           {/* <button className="flex items-center justify-center gap-2 px-4 py-2 border border-gray-300 rounded-lg hover:bg-gray-50 transition-colors duration-200 text-sm lg:text-base">
