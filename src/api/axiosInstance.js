c83ce501fa28d08619329b0bf0e544d7def418e7
hamza-mqbl <hamzaaliics@gmail.com>
2025-07-25 15:08:32 +0500

    changed the bg url

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Cookies from "js-cookie";
-
+//changed the backend url
 const axiosInstance = axios.create({
   baseURL: `${import.meta.env.VITE_API_BASE_URL}/api`,
   headers: {
