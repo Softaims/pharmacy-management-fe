88630d796cfaa1d7936dfb937b2fbd0c3384fc3f
hamza-mqbl <hamzaaliics@gmail.com>
2025-07-25 11:16:56 +0500

    change the color of thumb in toggle to red when off

--- src/components/Pharmacy/Settings.jsx
+++ src/components/Pharmacy/Settings.jsx
@@ -647,11 +647,23 @@ const Settings = () => {
                     onChange={() => handleToggle(key)}
                     className="sr-only peer"
                   />
-                  <div className="relative w-11 h-6 bg-gray-200 rounded-full peer  peer-focus:ring-blue-300  peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all  peer-checked:bg-[#069AA2] "></div>
-                  <span className="ms-3 text-sm font-medium text-gray-900 ">
+                  <div
+                    className="relative w-11 h-6 bg-gray-200 rounded-full
+                  peer-focus:ring-blue-300 
+                  peer-checked:bg-[#069AA2] 
+                  after:content-[''] after:absolute after:top-0.5 after:start-[2px] 
+                  after:bg-[#E9486C] peer-checked:after:bg-white
+                  after:border-gray-300 after:border after:rounded-full 
+                  after:h-5 after:w-5 after:transition-all 
+                  peer-checked:after:translate-x-full 
+                  rtl:peer-checked:after:-translate-x-full
+  "
+                  ></div>
+                  <span className="ms-3 text-sm font-medium text-gray-900">
                     {schedule[key]?.isOpen ? "Ouvert" : "Fermé"}
                   </span>
                 </label>
+
                 {schedule[key]?.isOpen && (
                   <div className="flex items-center gap-2 flex-wrap">
                     {schedule[key].timeSlots.map((slot, index) => (
