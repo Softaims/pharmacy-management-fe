4e0efb64d661c25cc169228ce686a0a6d874c306
hamza-mqbl <hamzaaliics@gmail.com>
2025-07-30 12:01:14 +0500

    Added the dynamic title for pharm and admin pages

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,13 +5,21 @@ import PharmacyDashboard from "../pages/Pharmacy/PharmacyDashboard.jsx";
 
 import ProtectedRoute from "./ProtectedRoute.jsx";
 import { useAuth } from "../contexts/AuthContext.jsx";
+import useDocumentTitle from "../components/useDocumentTitle.js";
 
 export default function AppRoutes() {
-  const { isAuthenticated, role, isLoading } = useAuth();
+  const { isAuthenticated, role } = useAuth();
 
   // if (isLoading) {
   //   return <div>Loading...</div>; // Or a proper loading component
   // }
+  useDocumentTitle(
+    isAuthenticated
+      ? role === "admin"
+        ? "Mézordopro | Accès admin"
+        : "Mézordopro | Accès pharmacien"
+      : "Mézordopro | Login"
+  );
 
   return (
     <Routes>
